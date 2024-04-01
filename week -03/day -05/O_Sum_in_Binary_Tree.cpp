718d6ec93ccc3f076f451002163e0981da8e9b12
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-04-01 21:25:52 +0600

    Add files via upload

---
+++ week -03/day -05/O_Sum_in_Binary_Tree.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define int long long
+#define float double
+#define endl '\n'
+using namespace std;
+/* 
+When I wrote this code ,
+only Allah and I knew what I did.
+   
+Now... only Allah knows
+*/
+
+int32_t main () 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        int sum = 0;
+        while(n>0) {
+            sum += n;
+            n=n/2;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
