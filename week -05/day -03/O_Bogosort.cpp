cd90cf631398593de7968c9149d1c9f640c0b4e3
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-04-21 11:35:45 +0600

    Create O_Bogosort.cpp

---
+++ week -05/day -03/O_Bogosort.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define int long long
+#define float double
+#define endl '\n'
+using namespace std;
+/* 
+When I wrote this code ,
+only Allah and I knew what I did.
+   
+Now... only Allah knows
+*/
+
+int32_t main () 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++) {
+            cin>>v[i];
+        }
+        sort(v.begin(),v.end(),greater<int> ());
+        for(int i=0;i<n;i++) {
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
