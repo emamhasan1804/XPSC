cf18fa5b55552209468b3bc90b3cd5ee50902cc3
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-05-04 00:45:45 +0600

    Create J_Divisible_Array.cpp

---
+++ week -07/day -02/J_Divisible_Array.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define int long long
+#define float double
+#define endl '\n'
+using namespace std;
+/* 
+When I wrote this code ,
+only Allah and I knew what I did.
+   
+Now... only Allah knows
+*/
+
+int32_t main () 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++) {
+            cout<<2*i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
