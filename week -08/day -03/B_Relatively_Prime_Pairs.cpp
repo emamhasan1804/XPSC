3c5afcf41505102cbac55a3c25bac822025b75fc
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-05-13 15:17:06 +0600

    Create B_Relatively_Prime_Pairs.cpp

---
+++ week -08/day -03/B_Relatively_Prime_Pairs.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define int long long
+#define float double
+#define endl '\n'
+using namespace std;
+/* 
+When I wrote this code ,
+only Allah and I knew what I did.
+   
+Now... only Allah knows
+*/
+
+int32_t main () 
+{
+    int l,r;
+    cin>>l>>r;
+    cout<<"YES"<<endl;
+    for(int i=l;i<=r;i+=2) {
+        cout<<i<<" "<<i+1<<endl;
+    }
+    return 0;
+}
