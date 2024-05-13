86452d87e2cb841928b2f336e1d37e0307adceae
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-05-13 16:39:48 +0600

    Create F_Maximum_GCD.cpp

---
+++ week -08/day -03/F_Maximum_GCD.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define int long long
+#define float double
+#define endl '\n'
+using namespace std;
+/* 
+When I wrote this code ,
+only Allah and I knew what I did.
+   
+Now... only Allah knows
+*/
+
+int32_t main () 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        cout<<n/2<<endl;
+    }
+    return 0;
+}
