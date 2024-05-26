508efda3ea465c9867df1a3d5e3a8faffb6fa20a
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-05-26 16:08:29 +0600

    Create B_Sliding_Window_Median.cpp

---
+++ week -10/day -03/Topicwise/B_Sliding_Window_Median.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+// template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int> v(n);
+    for(int i=0;i<n;i++) {
+        cin>>v[i];
+    }
+    int l=0,r=0;
+    pbds<pair<int,int>> p;
+    int ind = k/2;
+    if(k%2==0) ind--;
+    while(r<n) {
+        p.insert({make_pair(v[r],r)});
+        if(r-l+1==k) {
+            auto it = p.find_by_order(ind);
+            cout<<it->first<<" ";
+            p.erase(make_pair(v[l],l));
+            l++;
+            r++;
+        }
+        else {
+            r++;
+        }
+    }
+    return 0;
+}
