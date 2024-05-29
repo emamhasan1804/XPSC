0eaa5e25abb73ef749b60faee862aa81f30dd88f
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-05-29 21:49:00 +0600

    Create F_Maximum_Crossings_(Hard_Version).cpp

---
+++ week -10/day -05/Topicwise/F_Maximum_Crossings_(Hard_Version).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++) {
+            cin>>v[i];
+        }
+        pbds<int> p;
+        int ans = 0;
+        for(int i=n-1;i>=0;i--) {
+            ans+=p.order_of_key(v[i]+1);
+            p.insert(v[i]);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
