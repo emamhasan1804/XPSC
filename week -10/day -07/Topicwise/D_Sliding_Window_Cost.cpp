d51d5ef1ed33f8f7e8870a55aef945726e402395
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-05-31 22:31:20 +0600

    Update D_Sliding_Window_Cost.cpp

--- week -10/day -07/Topicwise/D_Sliding_Window_Cost.cpp
+++ week -10/day -07/Topicwise/D_Sliding_Window_Cost.cpp
@@ -3,8 +3,8 @@
 #include<ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 using namespace std;
-// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
-template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+// template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
 #define int long long
 #define float double
 #define endl '\n'
@@ -18,24 +18,23 @@ int32_t main()
     for(int i=0;i<n;i++) {
         cin>>v[i];
     }
-    pbds<int> p;
+    pbds<pair<int,int>> p;
     for(int i=0;i<k;i++) {
-        p.insert(v[i]);
+        p.insert({v[i],i});
     }
     int mid = ((k+1)/2)-1;
     auto it = p.find_by_order(mid);
-    int middle = *it;
+    int middle = it->first;
     int sum = 0;
-    for(int i:p) {
-        sum += abs(i-middle);
+    for(auto i:p) {
+        sum += abs(i.first-middle);
     }
     cout<<sum<<" ";
     int l=0,r=k;
     while(r<n) {
-        int indx = p.order_of_key(v[l]);
-        p.erase(p.find_by_order(indx));
-        p.insert(v[r]);
-        int middle1 = *p.find_by_order(mid);
+        p.erase({v[l],l});
+        p.insert({v[r],r});
+        int middle1 = (p.find_by_order(mid))->first;
         sum += abs(middle1-v[r]) - abs(middle-v[l]);
         if(k%2 == 0) {
             sum -= (middle1-middle);
