d99add39158cfd405db04e60ae027ec9636139ae
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-06-23 16:11:33 +0600

    Create M_Not Dividing.cpp

---
+++ week -13/day -02/M_Not Dividing.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++) {
+            cin>>v[i];
+            if(v[i]==1) v[i]++;
+        }
+        for(int i=1;i<n;i++) {
+            if(v[i]%v[i-1]==0) v[i]++;
+        }
+        for(int i:v) cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
