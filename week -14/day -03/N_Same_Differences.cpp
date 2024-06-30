58b9ac9c6ac25197f8dfc0bab185ab0c8912928a
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-07-01 02:10:40 +0600

    Create N_Same_Differences.cpp

---
+++ week -14/day -03/N_Same_Differences.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        map<int,int> mp;
+        for(int i=0;i<n;i++) {
+            cin>>v[i];
+            mp[v[i]-i]++;
+        }
+        int cnt = 0;
+        for(auto it:mp) {
+            cnt += ((it.second*(it.second-1))/2);
+        }
+        cout<<cnt<<endl;
+    }
+    return 0;
+}
