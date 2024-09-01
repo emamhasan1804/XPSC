0cccf0252c8e76d033d9ed4c80ef8a6d8c0d6774
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-09-01 19:51:40 +0600

    Create C_Binary_Imbalance.cpp

---
+++ week -19/day -02/C_Binary_Imbalance.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int cnt = 0;
+        for(char c:s) if(c=='0') cnt++;
+        if(cnt==0) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}
