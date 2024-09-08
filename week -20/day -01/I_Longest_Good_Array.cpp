d2dd8c35fc5c89b49a37ff9d9350822f7c27a506
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-09-08 22:00:49 +0600

    Create I_Longest_Good_Array.cpp

---
+++ week -20/day -01/I_Longest_Good_Array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int l,r;
+        cin>>l>>r;
+        int x = r-l;
+        int ans = ((sqrt(1+8* x)-1)/2)+1;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
