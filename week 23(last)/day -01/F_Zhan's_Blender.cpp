32d71de8d6bd5d122b9ec9420724eb86e54b4809
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-09-24 20:31:31 +0600

    Create F_Zhan's_Blender.cpp

---
+++ week 23(last)/day -01/F_Zhan's_Blender.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int cs =1;
+    int t_c;cin>>t_c;while(t_c--) 
+    {
+        int n;
+        cin>>n;
+        int x,y;
+        cin>>x>>y;
+        if(n==0) {
+            cout<<0<<endl;
+            continue;
+        }
+        x = min(x,y);
+        int ans = (n+x-1)/x;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
