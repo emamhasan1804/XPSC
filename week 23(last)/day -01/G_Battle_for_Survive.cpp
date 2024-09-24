091d1c4fe4a227aae6f2694f56652c54ffed0da8
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-09-24 20:32:18 +0600

    Create G_Battle_for_Survive.cpp

---
+++ week 23(last)/day -01/G_Battle_for_Survive.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int cs =1;
+    int t_c;cin>>t_c;while(t_c--) 
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++) {
+            cin>>v[i];
+        }
+        int x = v[n-2];
+        for(int i=0;i<n-2;i++) x-=v[i];
+        cout<<v[n-1]-x<<endl;
+    }
+    return 0;
+}
