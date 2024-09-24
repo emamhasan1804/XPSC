195b80e90380226daf9e4fa0d3f450ad43882820
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-09-24 19:58:36 +0600

    Create C_ Robin_Hood_and_the_Major_Oak.cpp

---
+++ week 23(last)/day -01/C_ Robin_Hood_and_the_Major_Oak.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // for set
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // for multiset
+#define int long long
+#define float double
+#define endl '\n'
+
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int cs =1;
+    int t_c;cin>>t_c;while(t_c--) 
+    {
+        int n,k;
+        cin>>n>>k;
+        int st = n-k+1;
+        int x = (n+1)/2;
+        x-=(st/2);
+        if(x%2) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}
